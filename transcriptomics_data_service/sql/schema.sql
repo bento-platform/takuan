3facb70980cb027713cbc65b1dc7b645a2088b5b
Julian <ornatus30@gmail.com>
2024-08-21 14:05:17 -0400

    add schema for gene expression

--- transcriptomics_data_service/sql/schema.sql
+++ transcriptomics_data_service/sql/schema.sql
@@ -3,3 +3,13 @@ CREATE TABLE IF NOT EXISTS experiment_results (
     assembly_id VARCHAR(63),
     assembly_name VARCHAR(63)
 );
+
+CREATE TABLE IF NOT EXISTS gene_expressions (
+    gene_code VARCHAR(31) NOT NULL,
+    sample_id VARCHAR(31) NOT NULL,
+    experiment_result_id VARCHAR(31) NOT NULL REFERENCES experiment_results ON DELETE CASCADE,
+    raw_count INTEGER NOT NULL,
+    tpm_count FLOAT,
+    tmm_count FLOAT,
+    PRIMARY KEY (gene_code, sample_id, experiment_result_id)
+);
