b7d5f02030819a34a425a7d2cab28244a85ce9a4
Julian <ornatus30@gmail.com>
2024-08-21 14:04:22 -0400

    add model for gen expression

--- transcriptomics_data_service/models.py
+++ transcriptomics_data_service/models.py
@@ -9,3 +9,12 @@ class ExperimentResult(BaseModel):
     experiment_result_id: str
     assembly_id: str | None = None
     assembly_name: str | None = None
+
+
+class GeneExpression(BaseModel):
+    gene_code: str
+    sample_id: str
+    experiment_result_id: str
+    raw_count: int
+    tpm_count: float | None = None
+    tmm_count: float | None = None
