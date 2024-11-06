34f886c892575b1535748377faa29a7777d16d5d
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2024-11-06 16:45:59 -0500

    perf: authz plugin conditional load

--- transcriptomics_data_service/authz/plugin.py
+++ transcriptomics_data_service/authz/plugin.py
@@ -1,6 +1,5 @@
 import importlib.util
-
-from fastapi import Request
+from types import ModuleType
 
 from transcriptomics_data_service.authz.middleware_base import BaseAuthzMiddleware
 from transcriptomics_data_service.config import get_config
@@ -8,16 +7,18 @@ from transcriptomics_data_service.logger import get_logger
 
 __all__ = ["authz_plugin"]
 
-
-def import_module_from_path(path):
-    spec = importlib.util.spec_from_file_location("authz_plugin", path)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-    return module
+config = get_config()
 
 
-AUTHZ_MODULE_PATH = "/tds/lib/authz.module.py"
-authz_plugin_module = import_module_from_path(AUTHZ_MODULE_PATH)
+def import_module_from_path(path) -> None | ModuleType:
+    if config.bento_authz_enabled:
+        spec = importlib.util.spec_from_file_location("authz_plugin", path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        return module.authz_middleware
+    else:
+        return None
+
 
 # Get the concrete authz middleware from the provided plugin module
-authz_plugin: BaseAuthzMiddleware = authz_plugin_module.authz_middleware
+authz_plugin = import_module_from_path("/tds/lib/authz.module.py")
