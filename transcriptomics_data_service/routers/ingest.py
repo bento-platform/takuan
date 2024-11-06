e7857e3e8964afa1417d1c4d312bc872d5282ded
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2024-11-06 15:06:25 -0500

    perf: log and return rcm duplicate errors

--- transcriptomics_data_service/routers/ingest.py
+++ transcriptomics_data_service/routers/ingest.py
@@ -1,9 +1,11 @@
+from logging import Logger
 from fastapi import APIRouter, File, HTTPException, Request, UploadFile, status
 import json
 from io import StringIO
 import pandas as pd
 
 from transcriptomics_data_service.db import DatabaseDependency
+from transcriptomics_data_service.logger import LoggerDependency
 from transcriptomics_data_service.models import ExperimentResult, GeneExpression
 
 __all__ = ["ingest_router"]
@@ -19,6 +21,7 @@ GENE_ID_KEY = "GeneID"
 )
 async def ingest(
     db: DatabaseDependency,
+    logger: LoggerDependency,
     experiment_result_id: str,
     assembly_name: str,
     assembly_id: str,
@@ -26,7 +29,7 @@ async def ingest(
 ):
     # Reading and converting uploaded RCM file to DataFrame
     file_bytes = rcm_file.file.read()
-    rcm_df = _load_csv(file_bytes)
+    rcm_df = _load_csv(file_bytes, logger)
 
     # Handling ingestion as a transactional operation
     async with db.transaction_connection() as transaction_con:
@@ -51,17 +54,24 @@ async def ingest(
     return {"message": "Ingestion completed successfully"}
 
 
-def _load_csv(file_bytes: bytes) -> pd.DataFrame:
+def _check_index_duplicates(index: pd.Index, logger: Logger):
+    duplicated = index.duplicated()
+    if duplicated.any():
+        dupes = index[duplicated]
+        err_msg = f"Found duplicated {index.name}: {dupes.values}"
+        logger.debug(err_msg)
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=err_msg)
+
+
+def _load_csv(file_bytes: bytes, logger: Logger) -> pd.DataFrame:
     buffer = StringIO(file_bytes.decode("utf-8"))
     buffer.seek(0)
     try:
         df = pd.read_csv(buffer, index_col=0, header=0)
 
         # Validating for unique Gene and Sample IDs
-        if df.index.duplicated().any():
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Duplicate Gene IDs detected.")
-        if df.columns.duplicated().any():
-            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Duplicate Sample IDs detected.")
+        _check_index_duplicates(df.index, logger)  # Gene IDs
+        _check_index_duplicates(df.columns, logger)  # Sample IDs
 
         # Ensuring raw count values are integers
         df = df.applymap(lambda x: int(x) if pd.notna(x) else None)
