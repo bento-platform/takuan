466c2aac82ed8e5409004be8fb098f58d2215541
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2024-11-14 17:18:53 -0500

    tests: db tests init and data files

---
+++ tests/test_db.py
@@ -0,0 +1,15 @@
+
+from transcriptomics_data_service.db import Database
+from transcriptomics_data_service.models import ExperimentResult
+
+
+async def test_create_experiment_result(db: Database, db_cleanup):
+    exp_id = "exp_test"
+    exp_result = ExperimentResult(
+        experiment_result_id=exp_id,
+        assembly_id="assembly_test_id",
+        assembly_name="assembly_test_name"
+    )
+    await db.create_experiment_result(exp_result)
+    db_exp_result = await db.read_experiment_result(exp_id)
+    assert exp_result == db_exp_result
