20b4a38db6a9ab33131abbe770f529e360fbd856
v-rocheleau <victor.rocheleau@mcgill.ca>
2024-11-18 15:18:21 -0500

    chore: include API key authz plugin for tests

--- local-tests.bash
+++ local-tests.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker compose -f docker-compose.test.yaml down
-docker compose -f docker-compose.test.yaml run --build tds
+docker compose -f docker-compose.test.yaml run --rm tds
 docker compose -f docker-compose.test.yaml down
 
-docker image prune -f
+docker rmi
